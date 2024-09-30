ef13e4a24d9e6fbbb4388684d648f9a8ab7d409b
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-09-30 02:25:23 +0200

    updated App.js

--- clean-template/src/App.js
+++ clean-template/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World!</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, suscipit
+        vel dolores dolorem quo molestiae voluptate saepe magni neque accusamus
+        at modi voluptas voluptatum ex molestias repellat expedita doloremque
+        esse tenetur non! Quasi sint, officia tempore facere maxime, ab quisquam
+        asperiores possimus quidem porro nobis! Voluptates facere unde ex
+        impedit.
+      </p>
     </div>
   );
 }
