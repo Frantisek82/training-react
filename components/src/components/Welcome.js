0bc3a25c21aaa253b541f7c70b97ad63885de587
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-10-03 10:42:18 +0200

    updated Welcome.js with NewDate

--- components/src/components/Welcome.js
+++ components/src/components/Welcome.js
@@ -1,9 +1,13 @@
 import "./Welcome.css";
+import NewDate from "./NewDate";
 
 function Welcome(props) {
+  const today = new Date(Date.now()).toLocaleDateString();
+
   return (
     <div>
       <h1>Hello, {props.name}!</h1>
+      <NewDate today={today}></NewDate>
     </div>
   );
 }
