1c49650f6167a1e8ff4427c7fb659fb34c1f4fe0
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-10-22 18:09:12 +0200

    updated clean-template

--- clean-template/src/App.js
+++ clean-template/src/App.js
@@ -1,18 +1,9 @@
 // import './App.css';
 
 function App() {
+  const numbers = ["John","James","Jane",]
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eius
-        omnis ducimus, sint nesciunt, reprehenderit modi atque ad aspernatur
-        dolores harum ipsum architecto optio perferendis officia neque
-        consequatur sunt aut porro quas ex aliquid. Magnam doloribus, est
-        dolorum autem, aliquid iste dicta id corrupti perspiciatis iure quia
-        suscipit minus accusantium?
-      </p>
-    </div>
+    
   );
 }
 
