71442897713ba25a23df33942c284272719c6f8f
Frantisek B <frantisekbabinsky.cz@gmail.com>
2024-10-25 00:06:03 +0200

    Timer

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,64 +1,8 @@
-import "./App.css";
-// // import { useState } from "react";
-// import Person from "./component/Person";
-
-// function App() {
-//   // const [count, setCount] = useState(0);
-
-//   return (
-//     <div>
-//       <Person />
-//       {/* <p>Has clicado {count} veces.</p> */}
-//       {/* <button onClick={() => setCount(count + 1)}>PULSAME</button> */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-//-----------------------------------------------------------------------------
-//Exercise
-//-----------------------------------------------------------------------------
-import Counter from "./component/Counter";
-import { useState } from "react";
+// import './App.css';
+import Timer from "./component/Timer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const handlePlusOne = () => {
-    setCount(count + 1);
-  };
-  const handleMenosOne = () => {
-    setCount(count - 1);
-  };
-  const handleTimesFive = () => {
-    if (count !== 0) {
-      setCount(count * 5);
-    } else {
-      alert("Anything multiplied by zero is equal to zero");
-    }
-  };
-  const handleDivideByHundred = () => {
-    if (count !== 0) {
-      setCount(count / 100);
-    } else {
-      alert("Dividing zero with any number will always give us a zero");
-    }
-  };
-
-  return (
-    <div>
-      <Counter count={count} />
-      <button onClick={handlePlusOne}>PULSAME PARA SUMAR</button>
-      <button onClick={handleMenosOne}>PULSAME PARA RESTAR</button>
-      <button onClick={handleTimesFive}>PULSAME PARA MULTIPLICAR POR 5</button>
-      <button onClick={handleDivideByHundred}>
-        PULSAME PARA DIVIDIR POR 100
-      </button>
-    </div>
-  );
+  return <Timer />;
 }
 
 export default App;
-
-//------------------------------------------------------------------------------
--- hooks/src/component/Counter.js
+++ hooks/src/component/Counter.js
@@ -1,7 +1,64 @@
 import "./Counter.css";
+// // import { useState } from "react";
+// import Person from "./component/Person";
 
-function Counter(props) {
-  return <div className="contador">Has clicado {props.count} veces</div>;
+// function App() {
+//   // const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <Person />
+//       {/* <p>Has clicado {count} veces.</p> */}
+//       {/* <button onClick={() => setCount(count + 1)}>PULSAME</button> */}
+//     </div>
+//   );
+// }
+
+// export default Counter;
+
+//-----------------------------------------------------------------------------
+//Exercise
+//-----------------------------------------------------------------------------
+import ShowCounter from "./ShowCounter";
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handlePlusOne = () => {
+    setCount(count + 1);
+  };
+  const handleMenosOne = () => {
+    setCount(count - 1);
+  };
+  const handleTimesFive = () => {
+    if (count !== 0) {
+      setCount(count * 5);
+    } else {
+      alert("Anything multiplied by zero is equal to zero");
+    }
+  };
+  const handleDivideByHundred = () => {
+    if (count !== 0) {
+      setCount(count / 100);
+    } else {
+      alert("Dividing zero with any number will always give us a zero");
+    }
+  };
+
+  return (
+    <div>
+      <ShowCounter count={count} />
+      <button onClick={handlePlusOne}>PULSAME PARA SUMAR</button>
+      <button onClick={handleMenosOne}>PULSAME PARA RESTAR</button>
+      <button onClick={handleTimesFive}>PULSAME PARA MULTIPLICAR POR 5</button>
+      <button onClick={handleDivideByHundred}>
+        PULSAME PARA DIVIDIR POR 100
+      </button>
+    </div>
+  );
 }
 
 export default Counter;
+
+//------------------------------------------------------------------------------
